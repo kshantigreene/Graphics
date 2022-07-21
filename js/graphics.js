b99be8d72c17998f16d0d08b7624204e639d8c88
kshantigreene <kshanti@gmail.com>
2022-07-21 15:22:34 -0400

    Added a particle sphere from the Intro to Graphics book. Added button to switch shapes.

--- js/graphics.js
+++ js/graphics.js
@@ -1,24 +1,105 @@
 import "./three.js"
-//From https://threejs.org/docs/index.html#manual/en/introduction/Creating-a-scene 
+
+var geometry;
+var object;
+var cube;
+var sphereOfPoints;
+
+//Set up the scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-cube.scale.set(2,1.5,2)
-
 camera.position.z = 5;
+const renderer = new THREE.WebGLRenderer();
 
+//set up the renderer
+var location=document.getElementById("three");
+renderer.setSize(window.innerWidth-100,window.innerHeight-100);
+location.appendChild( renderer.domElement );
+
+//set button to change shapes
+var button=document.getElementById("shape");
+button.onclick=switchShape;
+
+/* 
+Switches shape from cube to sphere of points or vice versa
+ */
+function switchShape(){
+	if(object != null){
+		if(object==cube){
+			scene.remove(cube);
+			scene.add( sphereOfPoints );
+			object=sphereOfPoints;
+		}else if(object==sphereOfPoints){
+			scene.remove(sphereOfPoints);
+			scene.add(cube);
+			object=cube;
+		}
+		
+	}
+}
+
+/** rotates the current object */
 function animate() {
 	requestAnimationFrame( animate );
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	object.rotation.x += 0.01;
+	object.rotation.y += 0.01;
 	renderer.render( scene, camera );
 }
+/**draws a cube */
+function drawCube(){
+	//First make geometry, then material, then finally a shape from geometry and material
+	geometry = new THREE.BoxGeometry( 1, 1, 1 );
+	let material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+	cube = new THREE.Mesh( geometry, material );
+}
+
+/**draws a particle cube */
+function drawParticleSphere(){
+	//Set up a particle sphere
+	let vertexCoords = new Float32Array([ 0,0,0, 1,0,0, 0,1,0 ]);//location of the sphere
+	let vertexAttrib = new THREE.BufferAttribute(vertexCoords, 3);
+	geometry = new THREE.BufferGeometry();
+	geometry.setAttribute( "position", vertexAttrib );
+
+	let pointsBuffer = new Float32Array( 30000 ); // 3 numbers per vertex!
+	let colorBuffer = new Float32Array( 30000 );
+	let i = 0;
+	while ( i < 10000 ) {
+		let x = 2*Math.random() - 1;
+		let y = 2*Math.random() - 1;
+		let z = 2*Math.random() - 1;
+		if ( x*x + y*y + z*z < 1 ) {
+			// only use points inside the unit sphere
+			pointsBuffer[3*i] = x;
+			pointsBuffer[3*i+1] = y;
+			pointsBuffer[3*i+2] = z;
+			colorBuffer[3*i] = 0.25 + 0.75*Math.random();
+			colorBuffer[3*i+1] = 0.25 + 0.75*Math.random();
+			colorBuffer[3*i+2] = 0.25 + 0.75*Math.random();
+			i++;
+		}
+	}
+	let pointsGeom = new THREE.BufferGeometry();
+	pointsGeom.setAttribute("position",
+	new THREE.BufferAttribute(pointsBuffer,3));
+	pointsGeom.setAttribute("color",
+	new THREE.BufferAttribute(colorBuffer,3));
+	geometry.setAttribute( "position", vertexAttrib );
+	//create material for points.
+	let pointsMat = new THREE.PointsMaterial( {
+		color: "white",
+		size: 2,
+		sizeAttenuation: false,
+		vertexColors: true
+		} );
+	//finally create the sphere with the geometry and material
+	sphereOfPoints = new THREE.Points( pointsGeom, pointsMat );
+	
+}
+
+//Render the shapes and then select the first shape. Start animation.
+drawCube();
+drawParticleSphere();
+object=cube;
+scene.add(cube);
 animate();
